70efcc5b197c8c5f20798d9fa4759569d215c9a5
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-03 11:32:32 -0200

    02 Começando com Arrays

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,35 +1,18 @@
 package br.com.bytebank.banco.teste;
 
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
 
+	// Array
 	public static void main(String[] args) {
 
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
+		int[] idades = new int[5]; // inicializa o array com 0
 
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		ContaPoupanca cp = new ContaPoupanca(33, 22);
-
-		System.out.println(cc);
-		System.out.println(cp);
-//		println(cc);
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
+		
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
 	}
-
-	static void println() {
-	}
-
-	static void println(int a) {
-	}
-
-	static void println(boolean valor) {
-	}
-
-	static void println(Object object) {
-
-	}
-
 }
