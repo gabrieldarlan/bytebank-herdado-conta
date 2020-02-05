36911c79987d0a404b1dade80d40f739c0b6a9d6
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-05 13:51:56 -0200

    List e LinkedList

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -36,7 +36,7 @@ public abstract class Conta {
 	 * @param valor
 	 * @throws SaldoInsuficienteException
 	 */
-	
+
 	public void saca(double valor) throws SaldoInsuficienteException {
 		if (this.saldo < valor) {
 			throw new SaldoInsuficienteException("Saldo insuficiente:" + this.saldo + ", Valor: " + valor);
@@ -80,9 +80,23 @@ public abstract class Conta {
 	public static int getTotal() {
 		return Conta.total;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Número: " + this.numero + " Agencia: " + this.agencia;
+	}
+
+	@Override
+	public boolean equals(Object ref) {
+		Conta outra = (Conta) ref;
+		
+		if (this.agencia != outra.agencia) {
+			return false;
+		}
+
+		if (this.numero != outra.numero) {
+			return false;
+		}
+		return true;
 	}
 }
