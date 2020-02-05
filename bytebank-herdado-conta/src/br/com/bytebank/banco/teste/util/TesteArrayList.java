40266af5783901c0d5880761f5694b0026096929
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-05 15:59:42 -0200

    // AULA 06 - 01 Autoboxing e Unboxing

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,7 +1,6 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -10,34 +9,33 @@ public class TesteArrayList {
 
 	public static void main(String[] args) {
 		ArrayList<Conta> lista = new ArrayList<Conta>();
-	
+
 		ContaCorrente cc = new ContaCorrente(22, 11);
 		lista.add(cc);
 		ContaCorrente cc2 = new ContaCorrente(22, 22);
 		lista.add(cc2);
-		
-		
+
 		System.out.println(lista.size());
-		
+
 		Conta ref = lista.get(0);
-		
+
 		System.out.println(ref.getNumero());
-		
+
 		System.out.println("Tamanho: " + lista.size());
 
 		lista.remove(0);
-		
+
 		System.out.println("Tamanho: " + lista.size());
 
 		ContaCorrente cc3 = new ContaCorrente(33, 311);
 		lista.add(cc3);
-		
+
 		ContaCorrente cc4 = new ContaCorrente(33, 322);
 		lista.add(cc4);
-		
+
 		for (Conta conta : lista) {
 			System.out.println(conta);
 		}
-		
+
 	}
 }
